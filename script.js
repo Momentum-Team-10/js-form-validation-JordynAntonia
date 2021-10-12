51c1081edb15005ae173353169e97a9469ad2a06
Jordyn Price <88625349+JordynAntonia@users.noreply.github.com>
2021-10-12 08:07:03 -0400

    commit

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-let form = document.getElementById
-let emailInput = document.getElementId
+let form = document.getElementById("login-form")
+let emailInput = document.getElementId('email')
 
 console.log(form)
 console.log(emailInput)
 
-form.addEventListener('submit')
+form.addEventListener('submit' , (event) => {
     event.preventDefault()
     validateEmail(emailInput)
+    console.log ('event object', event)
+    console.log ('form submitted!')
 
+function validateEmail(input) {
+    if input.value.includes('@')) {
+        input.classList.add('.invailid')
+    } else {
 
-function validateEmail(input)
-
+    }
+    console.log(input)
+}
