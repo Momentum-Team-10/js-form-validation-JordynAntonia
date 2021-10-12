7fe895e7c2cbdd6ee0ce2c9195ed9505d1b9e2e9
Jordyn Price <88625349+JordynAntonia@users.noreply.github.com>
2021-10-12 07:18:36 -0400

    commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Add validation!');
+let form = document.getElementById
+let emailInput = document.getElementId
+
+console.log(form)
+console.log(emailInput)
+
+form.addEventListener('submit')
+    event.preventDefault()
+    validateEmail(emailInput)
+
+
+function validateEmail(input)
+
